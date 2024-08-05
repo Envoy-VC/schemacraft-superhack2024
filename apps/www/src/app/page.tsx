a12821aba46d3d6b984a0a95c9ec44943c24f505
Vedant Chainani <vedantchainani1084@gmail.com>
2024-08-05 11:57:02 +0530

    chore: add eas config

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,12 +1,17 @@
+'use client';
+
 import React from 'react';
 
 import { Navbar } from '~/components';
+
+import { Input } from '~/components/ui/input';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <h1>Welcome to the home page!</h1>
+      <Input placeholder='Arshit' />
     </div>
   );
 };
