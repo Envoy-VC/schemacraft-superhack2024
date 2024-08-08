018968bd01ded409089032bb88a5a6e965ea9e81
Vedant Chainani <vedantchainani1084@gmail.com>
2024-08-08 22:10:17 +0530

    chore: add page boilerplate

---
+++ apps/www/src/app/(dashboard)/dashboard/explore-attestation/page.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React, { useState } from 'react';
+
+import { easConfig } from '~/lib/eas';
+import { useEthers } from '~/lib/hooks';
+
+import { AttestationViewer } from 'eas-uikit';
+import { useChainId } from 'wagmi';
+
+import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
+
+const ViewSchema = () => {
+  const { signer } = useEthers();
+  const chainId = useChainId();
+
+  const [uid, setUID] = useState<string>('');
+  const [searchUID, setSearchUID] = useState<string | null>(null);
+
+  return (
+    <div className='mx-auto flex w-full max-w-xl flex-col items-center justify-center gap-8'>
+      <div className='flex w-full flex-row items-center gap-2'>
+        <Input
+          className='w-full'
+          placeholder='Search for a attestation'
+          value={uid}
+          onChange={(e) => setUID(e.target.value)}
+        />
+        <Button
+          onClick={() => {
+            setSearchUID(uid);
+          }}
+        >
+          Search
+        </Button>
+      </div>
+      {searchUID ? (
+        <AttestationViewer
+          attestationUID={searchUID}
+          easContractAddress={easConfig[chainId]?.eas}
+          signer={signer}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default ViewSchema;
