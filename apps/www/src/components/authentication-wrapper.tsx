fef7f737302e836e0bba615a4c232152dac44f48
Vedant Chainani <vedantchainani1084@gmail.com>
2024-08-10 06:18:43 +0530

    chore: fix session wrapper

--- apps/www/src/components/authentication-wrapper.tsx
+++ apps/www/src/components/authentication-wrapper.tsx
@@ -6,7 +6,13 @@ import { getSession } from '~/lib/session';
 
 export const SignedIn = async ({ children }: PropsWithChildren) => {
   const session = await getSession();
-  if (session.expires < new Date().toISOString()) {
+  console.log(session);
+  if (
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- can be undefined
+    session.expires === undefined ||
+    session.expires < new Date().toISOString()
+  ) {
+    console.log(session.expires < new Date().toISOString());
     redirect('/auth');
   }
   return <>{children}</>;
